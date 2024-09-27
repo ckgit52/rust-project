b23f01af5450d5cc3d8f70266e1e27b5e148e9f1
chandan <chandan@webmobinfo.ch>
2024-09-27 12:33:40 +0530

    maded one time connection with mongoDB

--- src/db/marks_db.rs
+++ src/db/marks_db.rs
@@ -1,18 +1,28 @@
 use mongodb::{Client, Collection};
-use crate::models::Marks;
 use std::env;
+use once_cell::sync::Lazy;
+use tokio::sync::Mutex;
+use crate::models::marks::Marks;
 
-// This function establishes a connection to the MongoDB collection for Marks
+pub static MONGO_CLIENT: Lazy<Mutex<Option<Client>>> = Lazy::new(|| Mutex::new(None));
+
 pub async fn get_marks_collection() -> Collection<Marks> {
-    // MongoDB URI from the .env file
-    let mongo_uri = env::var("MONGODB_URI").expect("MONGODB_URI must be set");
+    let mut client_lock = MONGO_CLIENT.lock().await;
 
-    // Initialize MongoDB client
-    let client = Client::with_uri_str(&mongo_uri)
-        .await
-        .expect("Failed to initialize MongoDB client");
+    if client_lock.is_none() {
+        // MongoDB URI from the .env file
+        let mongo_uri = env::var("MONGODB_URI").expect("MONGODB_URI must be set");
 
-    // Access the database and the collection
+        // Initialize MongoDB client
+        let client = Client::with_uri_str(&mongo_uri)
+            .await
+            .expect("Failed to initialize MongoDB client");
+        
+        *client_lock = Some(client);
+    }
+
+    // Return the collection
+    let client = client_lock.as_ref().unwrap();
     let db = client.database("marks");
     db.collection::<Marks>("mark-list")
 }
